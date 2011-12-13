c1da52b5d33c2c98a80be14f1b49cd690acd43ce
Ben Zittlau <ben@zittlau.ca>
2011-12-12 22:22:35 -0700

    Rails project created

---
+++ mongoid_pres/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidPres::Application.config.secret_token = '9cb2f3796ba418bae0b7e841060822b371d07f65682aeb0b588639d0751f261e48f239c783bafe9a80a4295a1c608b78ddca17f685b4d9aa63623672d72481b9'
